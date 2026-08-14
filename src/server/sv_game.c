06c1e77532e2d8bc4104f691de8926ea2791eb49
mans-ahlander <37533073+mans-ahlander@users.noreply.github.com>
2026-08-14 15:10:38 +0900

    Fixed gentities NULL initialization bug at VM reset

--- src/server/sv_game.c
+++ src/server/sv_game.c
@@ -932,15 +932,16 @@ static void SV_InitGameVM( qboolean restart ) {
 	// start the entity parsing at the beginning
 	sv.entityParsePoint = CM_EntityString();
 
+	// Hoyo: This section need to be moved up before VM_call. Before I found this bug, it was already fixed by Nathan Baggs, Kornik, and even fixed in later Q3 Arena.
+	// clear all gentity pointers that might still be set from
+	// a previous level
+	for (i = 0; i < sv_maxclients->integer; i++) {
+		svs.clients[i].gentity = NULL;
+	}
+
 	// use the current msec count for a random seed
 	// init for this gamestate
 	VM_Call( gvm, GAME_INIT, svs.time, Com_Milliseconds(), restart );
-
-	// clear all gentity pointers that might still be set from
-	// a previous level
-	for ( i = 0 ; i < sv_maxclients->integer ; i++ ) {
-		svs.clients[i].gentity = NULL;
-	}
 }
 
 
