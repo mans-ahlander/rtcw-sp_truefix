3e00a543ed014a84f8ee484693a92ce69e44b139
mans-ahlander <37533073+mans-ahlander@users.noreply.github.com>
2026-08-15 13:32:24 +0900

    Fix out-of-bounds drawSurf access with multiple views

--- src/renderer/tr_main.c
+++ src/renderer/tr_main.c
@@ -1612,12 +1612,14 @@ void R_SortDrawSurfs( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 		return;
 	}
 
+	/* Hoyo. Moved to R_RenderView for proper total surface count in a frame
 	// if we overflowed MAX_DRAWSURFS, the drawsurfs
 	// wrapped around in the buffer and we will be missing
 	// the first surfaces, not the last ones
 	if ( numDrawSurfs > MAX_DRAWSURFS ) {
 		numDrawSurfs = MAX_DRAWSURFS;
 	}
+	*/
 
 	// sort the drawsurfs by sort type, then orientation, then shader
 	qsortFast( drawSurfs, numDrawSurfs, sizeof( drawSurf_t ) );
@@ -1834,6 +1836,7 @@ or a mirror / remote location
 */
 void R_RenderView( viewParms_t *parms ) {
 	int firstDrawSurf;
+	int numDrawSurfs; // Hoyo.
 
 	if ( parms->viewportWidth <= 0 || parms->viewportHeight <= 0 ) {
 		return;
@@ -1871,7 +1874,16 @@ void R_RenderView( viewParms_t *parms ) {
 
 	R_GenerateDrawSurfs();
 
-	R_SortDrawSurfs( tr.refdef.drawSurfs + firstDrawSurf, tr.refdef.numDrawSurfs - firstDrawSurf );
+	// Hoyo. Moved this section here from R_SortDrawSurfs. (Credit to iortcw team)
+	// if we overflowed MAX_DRAWSURFS, the drawsurfs
+	// wrapped around in the buffer and we will be missing
+	// the first surfaces, not the last ones
+	numDrawSurfs = tr.refdef.numDrawSurfs;
+	if (numDrawSurfs > MAX_DRAWSURFS) {
+		numDrawSurfs = MAX_DRAWSURFS;
+	}
+	
+	R_SortDrawSurfs( tr.refdef.drawSurfs + firstDrawSurf, numDrawSurfs - firstDrawSurf );
 
 	// draw main system development information (surface outlines, etc)
 	R_FogOff();
